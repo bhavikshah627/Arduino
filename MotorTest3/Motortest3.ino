c2fb52cc4ad49a3f94fbe2e0d57b1d32c9b4585b
Bhavik Shah <bhavikshah627@gmail.com>
2015-02-14 17:07:12 -0500

    Arduino Folder

---
+++ MotorTest3/Motortest3.ino
@@ -0,0 +1,12 @@
+#include ".../Libraries/Ourlib.h"
+
+void setup()
+{
+	Serial.begin(9600);
+	qik.init();
+}
+void loop()
+{
+	qik.setM0speed(127);
+	Serial.print(getRotations());
+}
---
+++ PingTest/PingTest.ino
@@ -0,0 +1,43 @@
+void setup() 
+{
+	Serial.begin(9600);
+	Serial.print("Starting...\n");
+
+}
+
+int getPingDist(int pingPin)
+{
+	// establish variables for duration of the ping, 
+	// and the distance result in inches and centimeters:
+	long duration, inches, cm;
+
+	pinMode(pingPin, OUTPUT);
+	digitalWrite(pingPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(pingPin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(pingPin, LOW);
+
+	pinMode(pingPin, INPUT);
+	duration = pulseIn(pingPin, HIGH);
+
+	cm = microsecondsToCentimeters(duration);
+
+	return cm;
+}
+
+
+long microsecondsToCentimeters(long microseconds)
+{
+	// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+	// The ping travels out and back, so to find the distance of the
+	// object we take half of the distance travelled.
+	return microseconds / 29 / 2;
+}
+
+void loop()
+{
+	int pingdist = getPingDist(7);
+	Serial.println(pingdist);
+	delay(25);
+}
