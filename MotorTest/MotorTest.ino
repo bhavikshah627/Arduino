1a07efe7dda5a3a34ae7822dfbe0b930fdfc1789
Bhavik Shah <bhavikshah627@gmail.com>
2015-02-28 15:57:24 -0500

    got rid of something stoopid
    it was pretty stoopid

--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -1,16 +1,12 @@
 
-extern int x;
-
 void setup()
 {
-	Serial.begin(9600);
-	//qik.init();
-        initMotors();
-       // encM0=0;
-        x=10;
+  Serial.begin(9600);
+  initMotors();
 }
 void loop()
-{
-	//qik.setM0speed(127);
-	Serial.print(getRotations());
+{   
+  setM0speed(50);
+  
+  Serial.println(getRotations());
 }
--- Motortest/OurLib.ino
+++ Motortest/OurLib.ino
@@ -3,15 +3,46 @@
 
 PololuQik2s12v10 qik(8, 9, 10); // Object of our Pololu Qik; hooked up to pins 8,9,10
 volatile long encM0 = 0;
-int x;
 
 void initMotors() //call this in your setup function or else no motors for anyone
 {
 	qik.init();
-	//attachInterrupt(0, EncoderA, RISING);
+	attachInterrupt(0, EncoderA, RISING);
+        encM0 = 0;
 }
 
-int getRotations()
+void EncoderA() //that danged encoder -- interupting everything
 {
-//	return encA / 4;
+  encM0++;
+}
+
+void stopMotors()
+{
+  qik.setM0Speed(0);
+  qik.setM1Speed(0);
+}
+
+void resetEncoder()
+{
+  encM0 = 0;
+}
+
+void setM0speed(int speed)
+{
+  qik.setM0Speed(speed);
+}
+
+void setM1speed(int speed)
+{
+  qik.setM1Speed(speed);
+}
+
+void setbothSpeeds(int s1, int s2)
+{
+  qik.setM0Speed(s1);
+  qik.setM1Speed(s2);
+}
+float getRotations()
+{
+  return (encM0/564);
 }
