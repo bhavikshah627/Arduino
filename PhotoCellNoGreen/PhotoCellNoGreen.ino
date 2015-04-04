9ec34a78634fa9f6f67ac5aa5e1197d69dcae2c7
William Cui <wyfcui@gmail.com>
2015-04-04 14:37:51 -0400

    tracing no green

---
+++ PhotoCellNoGreen/PhotoCellNoGreen.ino
@@ -0,0 +1,80 @@
+//#include <MotorLib.h>
+
+const int analogInPinOne = A0;
+const int analogInPinTwo = A2;
+const int analogInPinThree = A1;
+
+const int BLK_L = 90;
+const int WHT_L = 235;
+const int BLK_R = 60;
+const int WHT_R = 155;
+
+const int TRS_L = (BLK_L + WHT_L) / 2;
+const int TRS_R = (BLK_R + WHT_R) / 2;
+
+int LFT, MID, RHT;
+
+/*
+              Left  Middle  Right
+Black          80    18      68
+Black-Green
+Green          127   50      120
+Green-White
+White          177   87      178
+*/
+
+int getLFT() {
+  return analogRead(analogInPinOne);
+}
+int getMID() {
+  return analogRead(analogInPinTwo);
+}
+int getRHT() {
+  return analogRead(analogInPinThree);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  initMotors();
+}
+
+void loop()
+{
+  if (getRHT() < TRS_R)
+  {
+    if (getLFT() < TRS_L)
+    {
+      setbothSpeeds(30, 30);
+    }
+    else if (getLFT() > TRS_L)
+    {
+      setbothSpeeds(25, 0);
+      //right
+    }
+  }
+  else if (getRHT() > TRS_R)
+  {
+    if (getLFT() < TRS_L)
+    {
+      setbothSpeeds(0, 25);
+      //left
+    }
+    else if (getLFT() > TRS_L)
+    {
+      setbothSpeeds(30, 30);
+      //forward
+    }
+  }
+  else
+  {
+    setbothSpeeds(30, 30);
+    //forward
+  }
+
+  Serial.print(getLFT());
+  Serial.print(" ");
+  Serial.print(getMID());
+  Serial.print(" ");
+  Serial.println(getRHT());
+}
