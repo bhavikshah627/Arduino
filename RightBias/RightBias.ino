e98d271d5032a976d61c49622a7561525b57af21
Bhavik Shah <bhavikshah627@gmail.com>
2015-04-11 10:39:18 -0400

    Some changes to line tracing
    Deleted a random duplicate folder with line tracing in it
    added a const int for powers and for turn dir so nano doesn't waste time
    talking over serial

--- RightBias/RightBias.ino
+++ RightBias/RightBias.ino
@@ -4,15 +4,17 @@ const int analogInPinOne = A0;
 const int analogInPinTwo = A2;
 const int analogInPinThree = A1;
 
-const int BLK_L = 100;
-const int GRN_L = 170; 
-const int WHT_L = 235;
-const int BLK_M = 140;       
-const int GRN_M = 165;
-const int WHT_M = 390;
-const int BLK_R = 140;
-const int GRN_R = 200; 
-const int WHT_R = 270;
+const int BLK_L = 130;
+const int GRN_L = 200; 
+const int WHT_L = 275;
+
+const int BLK_M = 328;       
+const int GRN_M = 368;
+const int WHT_M = 440;
+
+const int BLK_R = 220;
+const int GRN_R = 250; 
+const int WHT_R = 345;
 
 const int TRS_L_1 = (BLK_L + GRN_L) / 2;
 const int TRS_L_2 = (GRN_L + WHT_L) / 2;
@@ -22,6 +24,10 @@ const int TRS_R_1 = (BLK_R + GRN_R) / 2;
 const int TRS_R_2 = (GRN_R + WHT_R) / 2;
 
 int LFT, MID, RHT;
+const int VEER_NONE = 0;
+const int VEER_LEFT  = 1;
+const int VEER_RIGHT  = 2;
+const int VEER_STRAIGHT = 3;
 
 
 int getLFT(){return analogRead(analogInPinOne);}
@@ -37,9 +43,12 @@ void setup()
 
 void loop() 
 {
+  static int state = VEER_NONE;
   int lftval = getLFT();
   int midval = getMID();
   int rhtval = getRHT();
+  const int  fpwr = 60;
+  const int  bpwr = -50;
   //exception for left green
   /*
   -
@@ -51,12 +60,18 @@ void loop()
   -
   
   */
-  if(rhtval < TRS_R_1)
-    setbothSpeeds(60, -40);
-  else if(lftval > TRS_L_2 && rhtval > TRS_R_2)
-    setbothSpeeds(40, 40);
-  else if(lftval < TRS_L_1 && rhtval >TRS_R_2)
-    setbothSpeeds(-40,60);
+  if(rhtval < TRS_R_1 && state!=VEER_RIGHT ){  //right sees black
+    setbothSpeeds(fpwr, bpwr);
+    state = VEER_RIGHT;
+  }
+  else if(lftval > TRS_L_2 && rhtval > TRS_R_2 && state!=VEER_STRAIGHT){  //left and right sees white
+    setbothSpeeds(fpwr, fpwr);
+    state=VEER_STRAIGHT;
+  }
+  else if(lftval < TRS_L_1 && rhtval >TRS_R_2 && state!=VEER_LEFT){ // left sees black and right sees white
+    setbothSpeeds(bpwr,fpwr);
+    state=VEER_LEFT;
+  }
   
   char buf[10];
   sprintf(buf, "%d %d %d", lftval, midval, rhtval);
