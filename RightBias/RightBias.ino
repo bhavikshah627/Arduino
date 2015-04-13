7d904486092184fc02a3c5a3e9482058c1d8e3a1
William Cui <wyfcui@gmail.com>
2015-04-13 16:18:59 -0400

    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb

--- RightBias/RightBias.ino
+++ RightBias/RightBias.ino
@@ -4,24 +4,28 @@ const int analogInPinOne = A0;
 const int analogInPinTwo = A2;
 const int analogInPinThree = A1;
 
-const int BLK_L = 110;
-const int GRN_L = 160; 
-const int WHT_L = 223;
+const int BLK_L = 225;
+const int GRN_L = 258; 
+const int WHT_L = 377;
 
-const int BLK_M = 220;       
-const int GRN_M = 300;
-const int WHT_M = 424;
+const int BLK_M = 550;       
+const int GRN_M = 485;
+const int WHT_M = 636;
 
-const int BLK_R = 230;
-const int GRN_R = 335; 
-const int WHT_R = 406;
+const int BLK_R = 329;
+const int GRN_R = 357; 
+const int WHT_R = 463;
 
-const int TRS_L_1 = (BLK_L + GRN_L) / 2;
-const int TRS_L_2 = (GRN_L + WHT_L) / 2;
-const int TRS_M_1 = (BLK_M + GRN_M) / 2;
-const int TRS_M_2 = (GRN_M + WHT_M) / 2;
-const int TRS_R_1 = (BLK_R + GRN_R) / 2;
-const int TRS_R_2 = (GRN_R + WHT_R) / 2;
+//const int TRS_L_1 = (BLK_L + GRN_L) / 2;
+//const int TRS_L_2 = (GRN_L + WHT_L) / 2;
+//const int TRS_M_1 = (BLK_M + GRN_M) / 2;
+//const int TRS_M_2 = (GRN_M + WHT_M) / 2;
+//const int TRS_R_1 = (BLK_R + GRN_R) / 2;
+//const int TRS_R_2 = (GRN_R + WHT_R) / 2;
+
+const int TRS_L = (BLK_L + WHT_L) / 2;
+const int TRS_M = (BLK_M + WHT_M) / 2;
+const int TRS_R = (BLK_R + WHT_R) / 2;
 
 int LFT, MID, RHT;
 
@@ -51,13 +55,14 @@ void loop()
   const int  bpwr = -30;
   //exception for left green
   
-  if(lftval > TRS_R_1 && lftval < TRS_R_2)
-    specDistTurn(90, fpwr, false);
-  else if(rhtval < TRS_R_1 && state != VEER_RIGHT )
+ // if(lftval > TRS_R_1 && lftval < TRS_R_2)
+   // specDistTurn(90, fpwr, false);
+  if(rhtval < TRS_R && state != VEER_RIGHT )
   {  //right sees black
-    if(midval<TRS_M_1)
+    if(midval < TRS_M)
     {
-      specDist(5, fpwr); 
+      specDist(1, fpwr); 
+      specDistTurn(10, fpwr, false);
     }
     else
     {
@@ -65,13 +70,13 @@ void loop()
     state = VEER_RIGHT;
     }
   }
-  else if(lftval > TRS_L_2 && rhtval > TRS_R_2 && state != VEER_STRAIGHT){  //left and right sees white
-    setbothSpeeds(fpwr, fpwr);
+  else if(lftval < TRS_L && rhtval > TRS_R && state != VEER_STRAIGHT){ //left sees black and right sees white
+    setbothSpeeds(bpwr, fpwr);
     state = VEER_STRAIGHT;
   }
-  else if(lftval < TRS_L_1 && rhtval > TRS_R_2 && state != VEER_LEFT){ // left sees black and right sees white
-    setbothSpeeds(bpwr,fpwr);
-    state = VEER_LEFT;
+  else if(lftval > TRS_L && rhtval > TRS_R && state != VEER_LEFT){ //left and right sees white
+    setbothSpeeds(fpwr,fpwr);
+    state = VEER_STRAIGHT;
   }
 //  else
 //  {
