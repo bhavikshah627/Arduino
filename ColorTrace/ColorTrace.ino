814b0af37d94c08bec0d04948758c5043d82b7f7
William Cui <wyfcui@gmail.com>
2015-04-17 17:00:12 -0400

    colorz

--- ColorTrace/ColorTrace.ino
+++ ColorTrace/ColorTrace.ino
@@ -2,22 +2,25 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-const int B_L = 930;
-const int G_L = 1567;
-const int W_L = 3358;
+const int B_L = 1890;
+const int G_L = 2510;
+const int W_L = 4650;
 
-const int B_M = 988;
-const int W_M = 3468;
+const int B_M = 1960;
+const int W_M = 4484;
 
-const int B_R = 1035;
-const int G_R = 1649;
-const int W_R = 3541;
+const int B_R = 2000;
+const int G_R = 2575;
+const int W_R = 4950;
 
-const int T_L = (B_L + W_L) / 2;
-//const int T_L_2 = (G_L + W_L) / 2;
+const int GREEN_GRD_L = 1000;
+const int GREEN_GRD_R = 1000;
+
+const int T_L_1 = (B_L + G_L) / 2;
+const int T_L_2 = (G_L + W_L) / 2;
 const int T_M = (B_M + W_M) / 2;
-const int T_R = (B_R + W_R) / 2;
-//const int T_R_2 = (G_R + W_R) / 2;
+const int T_R_1 = (B_R + G_R) / 2;
+const int T_R_2 = (G_R + W_R) / 2;
 
 int muxports[] = {1, 3};
 //left, middle, right;
@@ -27,13 +30,9 @@ Adafruit_TCS34725 tcsleft = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS
 
 void getMidC()
 {
-
   if (mux(0)) Serial.println("Yo");
   tcsleft.getRawDataEx(&mred, &mgreen, &mblue, &mclear);
- 
-
 }
-
 
 void getSideC()
 {
@@ -78,7 +77,7 @@ void setup()
     if (mux(muxports[i])) Serial.println("ERROR MUX");
     Serial.print("Init sensor ");
     Serial.println(muxports[i]);
-    if (tcsleft.begin()) 
+    if (tcsleft.begin())
     {
       Serial.println("Found sensor");
       tcsleft.setInterrupt(false);
@@ -92,17 +91,17 @@ void setup()
   if (mux(0)) Serial.println("ERROR MUX");
   Serial.print("Init sensor ");
   Serial.println(0);
-  if (tcsleft.begin()) 
+  if (tcsleft.begin())
   {
     Serial.println("Found sensor");
     tcsleft.setInterrupt(false);
   }
-  else 
+  else
   {
     Serial.println("No TCS34725 found ... check your connections");
     while (1); // halt!
   }
-  
+
   delay(60);
   initMotors();
   delay(1000);
@@ -118,27 +117,32 @@ void loop()
   getSideC();
   getMidC();
   char buf[40];
-  sprintf(buf, "%d, %d, %d", clear[0], mclear, clear[1]);
+  sprintf(buf, "%d, %d, %d, %d, %d", clear[0], mclear, clear[1], green[0], green[1]);
   Serial.println(buf);
-  if (clear[1] < T_R/*_1*/  && state != VEER_RIGHT )
+  if (clear[0] > T_L_1 && clear[0] < T_L_2 && green[0] > GREEN_GRD_L && state != VEER_LEFT) // left sees green
+  {
+    setbothSpeeds(bpwr, fpwr);
+    state = VEER_LEFT;
+  }
+  else if (clear[1] < T_R_1)
   { //right sees black
-    if (mclear < 1500)
+    if (mclear < T_M)
     {
       specDist(1, fpwr);
       specDistTurn(5, fpwr, false);
     }
-    else
+    else if (state != VEER_RIGHT)
     {
       Serial.println("RIGHT");
       setbothSpeeds(fpwr, bpwr);
       state = VEER_RIGHT;
     }
   }
-  else if (clear[0] < T_L/*_1*/  && clear[1] > T_R/*_2*/ && state != VEER_LEFT) { // left sees black and right sees white
+  else if (clear[0] < T_L_1  && clear[1] > T_R_2 && state != VEER_LEFT) { // left sees black and right sees white
     setbothSpeeds(bpwr, fpwr);
     state = VEER_LEFT;
   }
-  else if (clear[0] > T_L/*_2*/ && clear[1] > T_R/*_2*/ && state != VEER_STRAIGHT) { //left and right sees white
+  else if (clear[0] > T_L_2 && clear[1] > T_R_2 && state != VEER_STRAIGHT) { //left and right sees white
     setbothSpeeds(fpwr, fpwr);
     state = VEER_STRAIGHT;
   }
